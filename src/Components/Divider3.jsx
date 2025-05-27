54c2230031d7c00ce492f1eca6a1f118acc315fd
dionwsaja <dionwsaja@gmail.com>
2025-05-27 09:45:18 +0800

    second commit

--- src/Components/Divider3.jsx
+++ src/Components/Divider3.jsx
@@ -1,10 +1,10 @@
 const Divider3 = () => {
   return (
-    <section className="max-w-screen-3xl mx-auto mb-2 md:mb-20 pt-20">
+    <section className="max-w-screen-3xl mx-auto mb-2 md:mb-20 pt-20 ">
       <div className="relative">
-        <div className="absolute left-[50%] -translate-x-52 -translate-y-3 md:-translate-y-6 md:-translate-x-60 lg:-translate-x-[280px]">
-          <div className="flex justify-center bg-black w-[420px] md:w-[500px] items-center mx-auto px-4 lg:w-[600px]">
-            <p className="text-white text-md  md:text-2xl font-bold text-center lg:text-4xl">
+        <div className="absolute left-1/2 -translate-x-1/2 -translate-y-3 md:-translate-y-6 md:-translate-x-1/2 lg:-translate-x-1/2">
+          <div className="flex justify-center bg-black w-[300px] md:w-[500px] lg:w-[800px] items-center  px-4 ">
+            <p className="text-white text-md  md:text-2xl font-bold text-center lg:text-4xl ">
               <span className="text-slate-500 text-[12px] md:text-lg">
                 03.{" "}
               </span>
--- src/Components/Divider4.jsx
+++ src/Components/Divider4.jsx
@@ -4,8 +4,8 @@ const Divider4 = () => {
   return (
     <section className="max-w-screen-3xl mx-auto mb-2 md:mb-12 pt-16">
       <div className="relative">
-        <div className="absolute left-[50%] -translate-x-52 -translate-y-3 md:-translate-y-4 md:-translate-x-60 lg:-translate-x-[360px] lg:-translate-y-6">
-          <div className="flex justify-center bg-black w-[420px] md:w-[500px] items-center mx-auto px-4 lg:w-[720px]">
+        <div className="absolute left-1/2 -translate-x-1/2 -translate-y-3 md:-translate-y-4 md:-translate-x-1/2 lg:-translate-x-1/2 lg:-translate-y-6">
+          <div className="flex justify-center bg-black w-[300px] md:w-[500px] items-center mx-auto px-4 lg:w-[800px]">
             <p className="text-white text-md  md:text-2xl font-bold text-center lg:text-4xl">
               <span className="text-slate-500 text-[12px] md:text-lg">
                 04.{" "}
